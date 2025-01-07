e47f84abc7511e67d9d7ae1dd0e072f5cd3563b7
romainB <romain.jimmy.bergout@gmail.com>
2025-01-07 16:45:38 +0100

    Modif 07/01/2025

--- 07-01-25/Dom-Events/event.js
+++ 07-01-25/Dom-Events/event.js
@@ -1,4 +1,9 @@
 const title = document.querySelector("h1");
+const label = document.querySelector("label");
+const input = document.querySelector("input");
+
+document.body.style.display = "flex";
+document.body.style.flexDirection = "column";
 
 title.style.cursor = "pointer";
 title.style.width = "15%";
@@ -6,6 +11,12 @@ title.style.margin = "auto";
 title.style.marginTop = "20px";
 title.style.boxShadow = "0px 5px 10px 5px rgba(0,0,0,0.75)";
 title.style.textAlign = "center";
+
+label.style.textAlign = "center";
+label.style.marginTop = "40px";
+
+input.style.width = "20%";
+input.style.margin = "auto";
 
 title.addEventListener("click", function(){
     title.innerText = title.innerText == "D.O.M Events" ? "-- 🥳 --" : "D.O.M Events"; //?Definition en Test de condition ternaire
@@ -18,3 +29,17 @@ title.addEventListener("click", function(){
     //     title.innerText = "D.O.M Events"
     // }
 });
+
+input.addEventListener("focus", function(){
+    input.style.backgroundColor = "blue";
+    input.style.color = "white";
+});
+
+input.addEventListener("blur", function(){
+    input.style.backgroundColor = "";
+    input.style.color = "black";
+});
+
+
+
+
