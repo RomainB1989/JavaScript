b3c480b3320eb8ec17d827c99c0e187b54b3ae0c
romainB <romain.jimmy.bergout@gmail.com>
2025-01-07 15:57:10 +0100

    Modif 07/01/2025

--- 07-01-25/Dom-Events/event.js
+++ 07-01-25/Dom-Events/event.js
@@ -2,7 +2,7 @@ const title = document.querySelector("h1");
 
 title.style.cursor = "pointer";
 title.style.width = "15%";
-title.style.boxShadow = "10px 10px 5px 0px rgba(0,0,0,0.75)";
+title.style.boxShadow = "1px 1px 5px 5px rgba(0,0,0,0.75)";
 
 title.addEventListener("click", function(){
     if(title.innerText == "D.O.M Events"){
