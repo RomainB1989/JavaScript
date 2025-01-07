1863ae64753dd0f467d0fa87a667cf08646dd219
RomainB <romain.jimmy.bergout@gmail.com>
2025-01-07 10:57:57 +0100

    Delete Day 4 directory

--- Day 4/exo 03/test.js
+++
@@ -1,8 +0,0 @@
-const bar = document.querySelector(".bar");
-
-addEventListener("scroll", function(event){
-    const scrollMax = this.document.body.scrollHeight - this.innerHeight;
-    const where = this.window.scrollY / scrollMax * 100;
-    
-    bar.style.width = where + "%";
-});
