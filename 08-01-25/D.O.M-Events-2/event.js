2540c7134150bc0f9f8dd4b3153f986e0e8ff44f
romainB <romain.jimmy.bergout@gmail.com>
2025-01-08 11:32:50 +0100

    Modif DOM Event 2

--- 08-01-25/D.O.M-Events-2/event.js
+++ 08-01-25/D.O.M-Events-2/event.js
@@ -3,7 +3,7 @@ const div2 = document.querySelector("#div2");
 
 console.log(textarea);
 textarea.value = localStorage.getItem("text");
-//? div2.innerHTML = marked(localStorage.getItem("text")); Je n'arrive pas à récupérer le local storage pour le mettre dans la deuxième div.
+div2.innerHTML = textarea.value;
 
 textarea.addEventListener('keyup', function(event){
     console.log(textarea.value);
@@ -12,7 +12,6 @@ textarea.addEventListener('keyup', function(event){
 
 function addText(value){
     // div2.textContent = value; Version sans marked()
-
-    div2.innerHTML = marked(value);
     localStorage.setItem("text", value);
+    div2.innerHTML = marked(value);
 }
